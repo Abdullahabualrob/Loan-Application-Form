b4bedf3a3c2e061c47904dab8cc7d15e1daf3185
Abdillahabualrob <abed2004.aburub@gmail.com>
2025-11-02 00:32:27 +0200

    Add project code

---
+++ my-app/src/Form.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+const Form = () => {
+ 
+    const [form,setForm]=useState({ Name:"", phone:"", age:null,
+      isEmployee:false  , selectedOption:"less than 500$" ,isDisabled:true })
+    const handleSubmit=(e)=>{
+e.preventDefault();
+
+if(!form.isEmployee){
+    Swal.fire({
+        title: 'Error!',
+        text: 'Please fill in all required fields.',
+        icon: 'error',       
+        confirmButtonText: 'OK'
+      }); 
+      return;
+}
+
+Swal.fire({
+    title: 'Success!',
+    text: 'Form submitted successfully.',
+    icon: 'success',
+    confirmButtonText: 'OK'
+  });
+console.log("Form submitted");
+setForm({
+    Name: "",
+    phone: "",
+    age: null,
+    isEmployee: false,
+    selectedOption: "less than 500$",
+    isDisabled: true
+  });
+    };
+  
+
+   const handelbutton=()=>{
+    setForm({...form,isDisabled:false});
+
+   }
+    return (
+      <div className="Form">
+<form onSubmit={handleSubmit}>
+<h1>Requesting A Loan</h1> 
+<hr/>
+<br/>   
+<label>Name:</label>  <br/>     
+ <input type="text"  className="input" value={form.Name} onChange={(event)=>{
+    setForm({...form,Name:event.target.value});
+ }}/>
+ <br/>
+ <label>Phone Number:</label>  <br/>     
+ <input type="tel" className="input"  value={form.phone}  onChange={(event)=>{
+  setForm({...form,phone:event.target.value});  
+ }}/>
+ <br/>
+ <label>Age:</label>  <br/>     
+ <input type="number"  className="input" value={form.age} onChange={(event)=>{
+     const newAge = event.target.value;
+     setForm({ ...form, age: newAge });
+    if (form.Name.trim() !== "" && form.phone.trim() !== "" && newAge !== "") 
+        handelbutton();
+ }}/>
+ <br/>
+ <label>Are you an employee?</label>  <br/>     
+ <input type="checkbox"  className="check" checked={form.isEmployee} onChange={(event)=>{
+       setForm({...form,isEmployee:event.target.checked}); 
+ }}/>
+ <br/>
+ <br/>
+ <label>Salary:</label>  <br/>
+ <select className="input" value={form.selectedOption} onChange={(event)=>{
+      setForm({...form,selectedOption:event.target.value});  
+ }}>
+    <option value="less than 500$">less than 500$</option>
+    <option value="between 500$ and 2000$">between 500$ and 2000$</option>
+    <option value="above 2000$">above 2000$</option>
+ </select>
+ <br/>
+ <br/>
+<button type="submit" className="b"  disabled={form.isDisabled}
+
+>submit</button>
+</form>
+   
+    </div>
+  );
+};
+
+export default Form;
